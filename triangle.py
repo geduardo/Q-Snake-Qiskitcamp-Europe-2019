dcbbb079a6c93e13a19585b9d0bc1a508fb547e5
Lisa Tse <lisatse@Lisas-MacBook-Pro.local>
2019-09-13 12:22:17 +0200

    Fun mountain game added

---
+++ triangle.py
@@ -0,0 +1,30 @@
+import pew
+
+pew.init()
+screen = pew.Pix()
+
+for i in range(0,8):
+    screen.pixel(i,7,3)
+
+for i in range(1,5):
+    screen.pixel(8-i+1,8-i,3)
+    screen.pixel(i-1,8-i,3)
+
+loc_pixel = 0 
+screen.pixel(loc_pixel,0,1)
+while True:
+
+    keys = pew.keys()
+    print(keys)
+    if keys != 0:
+        if keys&pew.K_RIGHT:
+            screen.pixel(loc_pixel,0,0)
+            loc_pixel = loc_pixel +1
+            screen.pixel((loc_pixel)%8,0,1)
+        elif keys&pew.K_LEFT:
+            screen.pixel(loc_pixel,0,0)
+            loc_pixel = loc_pixel -1
+            screen.pixel((loc_pixel)%8,0,1)
+
+    pew.show(screen)
+    pew.tick(1/6)
