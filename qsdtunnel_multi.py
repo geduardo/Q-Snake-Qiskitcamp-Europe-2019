9927408828da43c00fd2d4baaf45c1c3258bce8d
jnbhlr <55276302+jnbhlr@users.noreply.github.com>
2019-09-30 16:17:11 +0200

    fixed issues, improved docs, new supply files!
    fixed issues:
    1) display probability (issue 9)
    2) improve point system (issue 10)
    
    improved docs:
    all the functions have improved (though not very detailed documentations)
    
    NEW SUPPLY FILE: pew_tunnel.py and pewblack.jpg

--- qsdtunnel_multi.py
+++ qsdtunnel_multi.py
@@ -1,5 +1,6 @@
-import pew
+import pew_tunnel as pew
 import random
+import pygame
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
 import numpy as np
 
@@ -7,10 +8,12 @@ import numpy as np
 #FUNCTIONS
 #########################################################################
 
+
+
 simulator = Aer.get_backend('statevector_simulator')
 
 def qrand(nbits):
-    "generates nbits real random numbers using quantum state measurements in qiskit."
+    """generates nbits real random numbers using quantum state measurements in qiskit."""
 
     circ = QuantumCircuit(1, 1)
     circ.h(0)
@@ -30,18 +33,23 @@ def qrand(nbits):
     return integ
 
 def Pt(U0, E, L, betac, gamma_sqc):
+    """return tunneling probability for square barrier"""
     return 1/ (np.cosh(betac * L)**2 + gamma_sqc * np.sinh(betac * L)**2)
 
 def beta(U0, E):
+    """supply function for Pt"""
     return np.sqrt(2* (U0 - E))
 
 def gamma_sq(U0, E):
+    """supply function for Pt"""
     return 0.25 * ((1 - E/U0)/(E/U0) + (E/U0)/(1-E/U0) - 2)
 
 def theta(p_tunnel):
+    """returns rotation angle corresponding to tunneling prob. p_tunnel"""
     return 2 * np.arcsin(np.sqrt(p_tunnel))
 
 def tunnelres(U0, length_snake, L, betac, gamma_sqc):
+    """returns 0 if tunnel, returns 1 if no tunnel"""
     P_t = Pt(U0, length_snake, L, betac, gamma_sqc) #get tunneling prob depending on current snake length
     theta_rot = theta(P_t) #get rot angle
 
@@ -55,7 +63,6 @@ def tunnelres(U0, length_snake, L, betac, gamma_sqc):
     res = job.result()
     vec = res.get_statevector()
     val = vec[0] * 1 + vec[1] * 0
-    print(P_t, theta_rot/(2*np.pi)*360)
     if val == 1:
         return 0
     else:
@@ -74,7 +81,7 @@ def tunnelres(U0, length_snake, L, betac, gamma_sqc):
 
 
 #initialize pew
-pew.init()
+dis = pew.init()
 screen = pew.Pix()
 #set size
 bits = 3
@@ -86,10 +93,24 @@ snake = [(2, 4)]
 dx, dy = 1, 0
 apple_x, apple_y = 6, 4
 screen.pixel(apple_x, apple_y, 2)
+howmanyapples = 1 #marker for total number of eaten apples, used for scoring
+
+
+#set graphics for probability display
+pygame.font.init()
+#gate backkgorund
+font1 = pygame.font.Font(None, 33)
+text = font1.render('Probability for tunneling is', True, (255, 0, 0))
+dis.blit(text, (20, 330))
+font2 = pygame.font.Font(None, 45)
+text2 = font2.render('100%', True, (255, 0, 0))
+dis.blit(text2, (130, 360))
+ima = pygame.image.load('pewblack.jpg')
+
 
 
 #tunneling parameters
-U0=37 #max snake length =36
+U0=37 #max snake length = 6x6 = 36
 E=1
 L=0.05 #optimal barrier size for nice tunneling probabilities
 
@@ -152,11 +173,9 @@ while True: #snake runs
         sx, sy = snake[-snakepos]
         E=len(snake)/2 #divide by two for lower tunnel prob for tail (lower mass->lower energy)
         tunnels = tunnelres(U0, E, L, beta(U0, E), gamma_sq(U0, E))
-        print('segment', snakepos, 'tunnels?', tunnels)
         if tunnels==1: #tunnels
             snakepos+=1
         else: #does not tunnel
-            print(snakepos, "does not tunnel")
             del snake[-snakepos]
             screen.pixel(sx, sy, 0)
 
@@ -169,15 +188,24 @@ while True: #snake runs
     if headtunnel==0 and (x, y) in bar:
         E=len(snake)
         tunnel = tunnelres(U0, E, L, beta(U0, E), gamma_sq(U0, E))
-        print('head tunnels?', tunnel)
         if tunnel==0 and len(snake) != 1: #head doesn't tunnel --> game over
             break
         else:
-            print('head tunnels')
             snakepos+=1
             headtunnel+=1
     elif headtunnel==1 and (x, y) in bar:
         headtunnel=0
+
+    #display tunneling prob.
+    E = len(snake)
+    if E > 1:
+        prob = Pt(U0, E, L, beta(U0, E), gamma_sq(U0, E))
+        text3 = font2.render(str(int(round(prob * 100))) + '%', True, (255, 0, 0))
+        dis.blit(ima, (130, 360))
+        dis.blit(text3, (130, 360))
+    else: #if length of snake ==1 (only head), tunneling prob = 100%
+        dis.blit(ima, (130, 360)) #cover the ultimate prob. display
+        dis.blit(text2, (130, 360)) #text2 = '100%'
     #####TUNNEL END
 
 
@@ -194,7 +222,8 @@ while True: #snake runs
             apple_x = qrand(bits)              #random.getrandbits(3) #use this for pseudo random number gen, no qiskit needed
             apple_y = qrand(bits)              #random.getrandbits(3)
         screen.pixel(apple_x, apple_y, 2)
-        game_speed += 0.2
+        game_speed += 0.2 #increase game speed
+        howmanyapples += 1 #increase number of eaten apples, score +1
     else:
         x, y = snake.pop(0)
         screen.pixel(x, y, 0)
@@ -206,7 +235,7 @@ for dx in range(-8, text.width):
     pew.show(screen)
     pew.tick(1 / 12)
 
-text = pew.Pix.from_text("Score:" + str(len(snake))) #Score message
+text = pew.Pix.from_text("Score:" + str(int(howmanyapples))) #Score message
 for dx in range(-8, text.width):
     screen.blit(text, -dx, 1)
     pew.show(screen)
